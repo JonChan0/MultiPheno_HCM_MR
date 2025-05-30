bc2fc8756e43b134d72b3e38cdf2eecdd3b6b68f
jon126 <jonathan.chan0@protonmail.com>
2025-05-30 15:35:01 +0800

    Update to use unidirectional MR for subgroup analyses for sig. MR results

--- 2_scripts/2_MR_iterator.sh
+++ 2_scripts/2_MR_iterator.sh
@@ -1,3 +1,4 @@
+
 #This script iterates over all the config files in ../input/configs to run the mendelian_randomisation.smk pipeline for each pair of traits
 
 module load Miniforge3/24.1.2-0
@@ -6,17 +7,21 @@ conda activate gms
 
 echo $(date)
 
+type=$1
 
 #This applies to running the bidirectional MR with HCMR GWAS of plasma proteins with HCM
-for file in ../1_data/input_configs/*.yaml
-do
-    echo "Carrying out bidirectional MR on ${file}"
-    snakemake --profile bmrc_profile_smk5  --snakefile 2_bidirectional_MR.smk --configfile $file --rerun-incomplete
-done
+if [[ "$type" == 'bidirectional' ]]; then
+    for file in ../1_data/input_configs/*.yaml
+    do
+        echo "Carrying out bidirectional MR on ${file}"
+        snakemake --profile bmrc_profile_smk5  --snakefile 2_bidirectional_MR.smk --configfile $file --rerun-incomplete
+    done
 
-
-
-
-
-
+elif [[ "$type" == 'subgroup' ]]; then
+    for file in ../1_data/input_configs/subgroup_analyses/*.yaml
+    do
+        echo "Carrying out UNIdirectional MR on ${file}"
+        snakemake --profile bmrc_profile_smk5  --snakefile 2_unidirectional_MR.smk --configfile $file --rerun-incomplete
+    done
+fi
 
